b449f3cf19076b3b21415a6fd8bcbb5ddb5cdf66
panagos <panosgeo11@gmail.com>
2026-01-17 19:13:32 +0200

    Add Color constructor with optional alpha parameter

--- vinter-engine/include/vinter/color.hpp
+++ vinter-engine/include/vinter/color.hpp
@@ -6,7 +6,14 @@ namespace vn {
     struct Color {
         std::uint8_t r, g, b, a;
 
-        Color(std::uint8_t red, std::uint8_t green, std::uint8_t blue, std::uint8_t alpha = 255);
+        Color(
+            const std::uint8_t red,
+            const std::uint8_t green,
+            const std::uint8_t blue,
+            const std::uint8_t alpha = 255
+        )
+            : r(red), g(green), b(blue), a(alpha) {
+        }
     };
 
     namespace colors {
