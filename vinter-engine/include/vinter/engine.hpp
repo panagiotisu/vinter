f8bf291ecbe15d6f13e6c66f19277a2bcdce4a1a
panagos <panosgeo11@gmail.com>
2026-01-17 12:32:20 +0200

    Define naive Engine API

--- vinter-engine/include/vinter/engine.hpp
+++ vinter-engine/include/vinter/engine.hpp
@@ -3,6 +3,18 @@
 namespace vn {
     class Engine {
     public:
+        Engine();
+        virtual ~Engine();
+
+        void run();
+
+    protected:
+        virtual void load() {}
+        virtual void poll_events() {}
+        virtual void update() {}
+        virtual void render() {}
+
     private:
+        bool m_running { false };
     };
 } // vn
--- vinter-engine/src/engine.cpp
+++ vinter-engine/src/engine.cpp
@@ -1,5 +1,18 @@
 #include "vinter/engine.hpp"
 
 namespace vn {
+    Engine::Engine() {}
 
+    Engine::~Engine() {}
+
+    void Engine::run() {
+        m_running = true;
+
+        load();
+        while (m_running) {
+            poll_events();
+            update();
+            render();
+        }
+    }
 } // vn
