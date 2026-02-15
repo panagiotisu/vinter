cd3453e2efd96ee8a9a6010ff932196fde934ac6
panagos <panosgeo11@gmail.com>
2026-02-15 12:49:20 +0200

    Make deadzones work with references of the axis values instead of glm::vec2s

--- vinter-engine/include/vinter/input/deadzones.hpp
+++ vinter-engine/include/vinter/input/deadzones.hpp
@@ -1,18 +1,16 @@
 #pragma once
 
-#include <glm/glm.hpp>
-
 // Various deadzone-applying algorithms based on the following article:
 // https://github.com/Minimuino/thumbstick-deadzones/blob/master/README.md
 
 namespace vn::deadzone {
-    float axial(float axis_1d, float deadzone);
+    void axial(float& axis, float& deadzone);
 
-    glm::vec2 axial(glm::vec2 axis_2d, float deadzone);
+    void axial(float& axis_x, float& axis_y, float deadzone);
 
-    glm::vec2 radial(glm::vec2 axis_2d, float deadzone);
+    void radial(float& axis_x, float& axis_y, float deadzone);
 
-    float smooth(float axis_1d, float deadzone);
+    void smooth(float& axis, float deadzone);
 
-    glm::vec2 smooth(glm::vec2 axis_2d, float deadzone);
+    void smooth(float& axis_x, float& axis_y, float deadzone);
 } // vn::deadzone
--- vinter-engine/src/input/deadzones.cpp
+++ vinter-engine/src/input/deadzones.cpp
@@ -1,22 +1,23 @@
 #include "vinter/input/deadzones.hpp"
 
+#include <cmath>
+
 namespace vn::deadzone {
-    float axial(const float axis_1d, const float deadzone) {
-        if (axis_1d < deadzone) return 0.f;
-        return axis_1d;
+    void axial(float& axis, const float deadzone) {
+        if (axis < deadzone) axis = 0.f;
     }
 
-    glm::vec2 axial(const glm::vec2 axis_2d, const float deadzone) {
-        return glm::vec2 {
-            axial(axis_2d.x, deadzone),
-            axial(axis_2d.y, deadzone)
-        };
+    void axial(float& axis_x, float& axis_y, const float deadzone) {
+        axial(axis_x, deadzone);
+        axial(axis_y, deadzone);
     }
 
-    glm::vec2 radial(const glm::vec2 axis_2d, const float deadzone) {
-        const float magnitude_squared = glm::dot(axis_2d, axis_2d);
-        if (magnitude_squared < deadzone * deadzone) return glm::vec2 { 0.f, 0.f };
-        return axis_2d;
+    void radial(float& axis_x, float& axis_y, const float deadzone) {
+        const float magnitude_squared = axis_x * axis_x + axis_y * axis_y;
+        if (magnitude_squared < deadzone * deadzone) {
+            axis_x = 0.f;
+            axis_y = 0.f;
+        }
     }
 
     static float map_range(
@@ -27,17 +28,22 @@ namespace vn::deadzone {
         return (new_min + (new_max - new_min) * (value - old_min) / (old_max - old_min));
     }
 
-    float smooth(const float axis_1d, const float deadzone) {
-        if (axis_1d < deadzone) return 0.f;
-        return map_range(axis_1d, deadzone, 1.f, 0.f, 1.f);
+    void smooth(float& axis, const float deadzone) {
+        axial(axis, deadzone);
+        axis = map_range(axis, deadzone, 1.f, 0.f, 1.f);
     }
 
-    glm::vec2 smooth(const glm::vec2 axis_2d, const float deadzone) {
-        const float magnitude_squared = glm::dot(axis_2d, axis_2d);
-        if (magnitude_squared < deadzone * deadzone) return glm::vec2 { 0.f, 0.f };
+    void smooth(float& axis_x, float& axis_y, const float deadzone) {
+        const float magnitude_squared = axis_x * axis_x + axis_y * axis_y;
+        if (magnitude_squared < deadzone * deadzone) {
+            axis_x = 0.f;
+            axis_y = 0.f;
+        }
 
         const float magnitude = std::sqrt(magnitude_squared);
-        const glm::vec2 axis_2d_normalized = axis_2d / magnitude;
-        return axis_2d_normalized * map_range(magnitude, deadzone, 1.f, 0.f, 1.f);
+        const float remapped_magnitude = map_range(magnitude, deadzone, 1.f, 0.f, 1.f);
+
+        axis_x *= remapped_magnitude / magnitude;
+        axis_y *= remapped_magnitude / magnitude;
     }
 } //vn::deadzone
