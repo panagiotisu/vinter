998d929b9eb7eed83a144809c3b86e6db1a9d346
panagos <panosgeo11@gmail.com>
2026-06-06 19:55:11 +0300

    Add colored logs

--- vinter-engine/include/vinter/logger.hpp
+++ vinter-engine/include/vinter/logger.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 namespace vn {
+
     class Logger {
     public:
         enum class Level : std::uint8_t {
@@ -18,36 +19,54 @@ namespace vn {
             m_level_filter = level;
         }
 
+        static void enable_colors(bool enabled) {
+            m_use_colors = enabled;
+        }
+
         template <typename... Args>
         static void debug(std::format_string<Args...> fmt, Args&&... args) {
-            log(Level::Debug, "[DEBUG]: ", fmt, std::forward<Args>(args)...);
+            log(Level::Debug, "\033[90m", "[DEBUG]", fmt, std::forward<Args>(args)...);
         }
 
         template <typename... Args>
         static void info(std::format_string<Args...> fmt, Args&&... args) {
-            log(Level::Info, "[INFO]: ", fmt, std::forward<Args>(args)...);
+            log(Level::Info, "\033[32m", "[INFO]", fmt, std::forward<Args>(args)...);
         }
 
         template <typename... Args>
         static void warning(std::format_string<Args...> fmt, Args&&... args) {
-            log(Level::Warning, "[WARNING]: ", fmt, std::forward<Args>(args)...);
+            log(Level::Warning, "\033[33m", "[WARNING]", fmt, std::forward<Args>(args)...);
         }
 
         template <typename... Args>
         static void error(std::format_string<Args...> fmt, Args&&... args) {
-            log(Level::Error, "[ERROR]: ", fmt, std::forward<Args>(args)...);
+            log(Level::Error, "\033[31m", "[ERROR]", fmt, std::forward<Args>(args)...);
         }
 
     private:
         template <typename... Args>
         static void
-        log(Level level, const char* prefix, std::format_string<Args...> fmt, Args&&... args) {
+        log(Level level,
+            const char* color,
+            const char* label,
+            std::format_string<Args...> fmt,
+            Args&&... args) {
             if (level < m_level_filter) {
                 return;
             }
-            std::cout << prefix << std::format(fmt, std::forward<Args>(args)...) << "\n";
+
+            const auto message = std::format(fmt, std::forward<Args>(args)...);
+
+            if (m_use_colors) {
+                std::cout << color << label << ": " << message << "\033[0m"
+                          << "\n";
+            } else {
+                std::cout << label << ": " << message << "\n";
+            }
         }
 
         inline static Level m_level_filter {Level::Info};
+        inline static bool m_use_colors {true};
     };
+
 } // namespace vn
