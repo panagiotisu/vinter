009e49239a40f1c3aba609adcfee2b934973a49a
panagos <panosgeo11@gmail.com>
2026-07-04 21:26:46 +0300

    Improve error reporting with colors

--- vinter-engine/include/vinter/panic.hpp
+++ vinter-engine/include/vinter/panic.hpp
@@ -12,34 +12,48 @@
     #define VN_DEBUG_BREAK() std::abort()
 #endif
 
-#define VN_FATAL(fmt, ...)                                                            \
-    do {                                                                              \
-        std::cerr << std::format(                                                     \
-            "[FATAL]\nFile: {}\nLine: {}\n" fmt "\n", __FILE__, __LINE__, __VA_ARGS__ \
-        );                                                                            \
-        VN_DEBUG_BREAK();                                                             \
-        std::abort();                                                                 \
+// ANSI color codes for red text
+#define VN_RED   "\033[31m"
+#define VN_RESET "\033[0m"
+
+#define VN_FATAL(fmt, ...)                   \
+    do {                                     \
+        std::cerr << VN_RED                  \
+                  << std::format(            \
+                         "[FATAL] " fmt "\n" \
+                         "File: {}\n"        \
+                         "Line: {}\n",       \
+                         __VA_ARGS__,        \
+                         __FILE__,           \
+                         __LINE__            \
+                     )                       \
+                  << VN_RESET;               \
+        VN_DEBUG_BREAK();                    \
+        std::abort();                        \
     } while (0)
 
 #ifdef NDEBUG
     #define VN_ASSERT(...) ((void)0)
 
 #else
-    #define VN_ASSERT(cond, fmt, ...)      \
-        do {                               \
-            if (!(cond)) {                 \
-                std::cerr << std::format(  \
-                    "[ASSERT] {}\n"        \
-                    "File: {}\n"           \
-                    "Line: {}\n" fmt "\n", \
-                    #cond,                 \
-                    __FILE__,              \
-                    __LINE__,              \
-                    ##__VA_ARGS__          \
-                );                         \
-                VN_DEBUG_BREAK();          \
-                std::abort();              \
-            }                              \
+    #define VN_ASSERT(cond, fmt, ...)                 \
+        do {                                          \
+            if (!(cond)) {                            \
+                std::cerr << VN_RED                   \
+                          << std::format(             \
+                                 "[ASSERT] " fmt "\n" \
+                                 "Condition: {}\n"    \
+                                 "File: {}\n"         \
+                                 "Line: {}\n",        \
+                                 ##__VA_ARGS__,       \
+                                 #cond,               \
+                                 __FILE__,            \
+                                 __LINE__             \
+                             )                        \
+                          << VN_RESET;                \
+                VN_DEBUG_BREAK();                     \
+                std::abort();                         \
+            }                                         \
         } while (0)
 
 #endif
