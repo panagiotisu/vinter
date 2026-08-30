bb30dbc55c669e05a14327698471e0bc75a30d16
panagos <panosgeo11@gmail.com>
2026-08-30 12:20:47 +0300

    Add sprite and sprite animation components and systems

---
+++ vinter-engine/include/vinter/graphics/sprite.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <string>
+
+#include <glm/glm.hpp>
+
+#include "vinter/graphics/texture.hpp"
+
+namespace vn {
+    struct Renderer;
+    struct ECS;
+
+    namespace component {
+        struct Sprite {
+            Texture texture {};
+            glm::vec2 frame_size {};
+            std::size_t frames_per_row { 1 };
+            std::size_t frames_per_col { 1 };
+            std::size_t current_frame {};
+            glm::bvec2 flip {};
+
+            [[nodiscard]]
+            static Sprite create(
+                Texture texture = {},
+                std::size_t frames_per_row = 1,
+                std::size_t frames_per_col = 1
+            );
+        };
+    } // namespace component
+
+    struct SpriteAnimation {
+        float frame_time_elapsed {};
+        float fps { 3 };
+        std::size_t start_frame {};
+        std::size_t end_frame {};
+        bool looping {};
+
+        friend bool operator==(const SpriteAnimation& lhs, const SpriteAnimation& rhs) {
+            return std::abs(lhs.frame_time_elapsed - rhs.frame_time_elapsed) < 1e-6
+                   && lhs.start_frame == rhs.start_frame && lhs.end_frame == rhs.end_frame
+                   && lhs.looping == rhs.looping;
+        }
+    };
+
+    namespace component {
+        struct SpriteAnimator {
+            std::unordered_map<std::string, SpriteAnimation> animations {};
+            SpriteAnimation* current_animation {};
+            float speed_scale = 1.f;
+
+            void
+            add(const std::string& name,
+                std::size_t start_frame = 0,
+                std::size_t end_frame = 0,
+                float fps = 3,
+                bool looping = false);
+
+            void play(Sprite& sprite, const std::string& animation_name);
+
+            void remove(const std::string& name);
+        };
+    } // namespace component
+
+    namespace system {
+        void update_sprite_animations(ECS& ecs, float delta);
+        void draw_sprites(ECS& ecs, const Renderer& renderer);
+    } // namespace system
+} // namespace vn
---
+++ vinter-engine/src/graphics/sprite.cpp
@@ -0,0 +1,123 @@
+#include "vinter/graphics/sprite.hpp"
+
+#include "vinter/ecs.hpp"
+#include "vinter/graphics/renderer.hpp"
+#include "vinter/spatial/geometry.hpp"
+#include "vinter/spatial/transform.hpp"
+
+namespace vn::component {
+    Sprite Sprite::create(Texture texture, std::size_t frames_per_row, std::size_t frames_per_col) {
+        return {
+            .texture = texture,
+            .frame_size = { static_cast<float>(texture.get_width())
+                                / static_cast<float>(frames_per_row),
+                            static_cast<float>(texture.get_height())
+                                / static_cast<float>(frames_per_col) },
+            .frames_per_row = frames_per_row,
+            .frames_per_col = frames_per_col,
+        };
+    }
+
+    void SpriteAnimator::add(
+        const std::string& animation_name,
+        std::size_t start_frame,
+        std::size_t end_frame,
+        float fps,
+        bool looping
+    ) {
+        animations.insert_or_assign(
+            animation_name,
+            SpriteAnimation {
+                .fps = fps,
+                .start_frame = start_frame,
+                .end_frame = end_frame,
+                .looping = looping,
+            }
+        );
+    }
+
+    void SpriteAnimator::play(Sprite& sprite, const std::string& animation_name) {
+        auto it = animations.find(animation_name);
+        if (it == animations.end()) {
+            return;
+        }
+        SpriteAnimation* desired_animation = &it->second;
+
+        if (current_animation == nullptr || current_animation != desired_animation) {
+            current_animation = desired_animation;
+            sprite.current_frame = current_animation->start_frame;
+            current_animation->frame_time_elapsed = 0.f;
+        }
+    }
+
+    void SpriteAnimator::remove(const std::string& animation_name) {
+        auto it = animations.find(animation_name);
+        if (it == animations.end()) {
+            return;
+        }
+        if (current_animation == &it->second) {
+            current_animation = nullptr;
+        }
+        animations.erase(it);
+    }
+} // namespace vn::component
+
+namespace vn::system {
+    void update_sprite_animations(ECS& ecs, float delta) {
+        ecs.view<component::Sprite, component::SpriteAnimator>().for_each(
+            [&](component::Sprite& sprite, component::SpriteAnimator& animator) {
+                float frame_duration = 1.f / animator.current_animation->fps;
+                animator.current_animation->frame_time_elapsed += delta * animator.speed_scale;
+
+                // Only change frame if current frame is finished.
+                if (animator.current_animation->frame_time_elapsed < frame_duration) {
+                    return;
+                }
+                sprite.current_frame += 1;
+                animator.current_animation->frame_time_elapsed = 0.f;
+
+                if (sprite.current_frame <= animator.current_animation->end_frame) {
+                    return;
+                }
+                // If animation reached the last frame and looping is enabled, reset to first.
+                if (animator.current_animation->looping) {
+                    sprite.current_frame = animator.current_animation->start_frame;
+                } else {
+                    // Latch to last frame if not looping.
+                    sprite.current_frame = animator.current_animation->end_frame;
+                }
+            }
+        );
+    }
+
+    void draw_sprites(ECS& ecs, const Renderer& renderer) {
+        ecs.view<component::Sprite, component::Transform>().for_each(
+            [&](const component::Sprite& sprite, const component::Transform& transform) {
+                const AABB src_aabb = {
+                    .position = { static_cast<float>(sprite.current_frame % sprite.frames_per_row)
+                                      * sprite.frame_size.x,
+                                  static_cast<float>(sprite.current_frame)
+                                      / static_cast<float>(sprite.frames_per_row)
+                                      * sprite.frame_size.y },
+                    .size = sprite.frame_size,
+                };
+
+                const AABB dest_aabb = {
+                    .position = { transform.global.position.x, transform.global.position.y },
+                    .size = src_aabb.size * transform.global.scale,
+                };
+
+                const glm::vec2 pivot = { dest_aabb.size.x / 2, dest_aabb.size.y / 2 };
+
+                renderer.draw_texture(
+                    sprite.texture,
+                    src_aabb,
+                    dest_aabb,
+                    transform.global.rotation,
+                    pivot,
+                    sprite.flip
+                );
+            }
+        );
+    }
+} // namespace vn::system
